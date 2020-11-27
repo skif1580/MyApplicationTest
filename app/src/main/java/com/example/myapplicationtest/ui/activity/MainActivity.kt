be2d9788c48d54a4a9c3cebc8ea078f8176b94ba
Xanter <skif1580.2@gmail.com>
2020-11-27 18:54:11 +0200

    add clickListener to MoviesListFragment.kt

--- app/src/main/java/com/example/myapplicationtest/ui/activity/MainActivity.kt
+++ app/src/main/java/com/example/myapplicationtest/ui/activity/MainActivity.kt
@@ -4,18 +4,29 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.fragment.app.FragmentTransaction
 import com.example.myapplicationtest.R
+import com.example.myapplicationtest.ui.fragment.ClickMovies
+import com.example.myapplicationtest.ui.fragment.MovieDetailsFragment
 import com.example.myapplicationtest.ui.fragment.MoviesListFragment
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), ClickMovies {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
         if (savedInstanceState == null) {
             supportFragmentManager.beginTransaction()
                 .replace(R.id.fl_container, MoviesListFragment.newInstance())
                 .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
-                .addToBackStack("panel").commit()
+                .commit()
         }
+    }
+
+    override fun clickMoviesListener() {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fl_container, MovieDetailsFragment.newInstance())
+            .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
+            .addToBackStack("panel")
+            .commit()
     }
 }
--- app/src/main/java/com/example/myapplicationtest/ui/fragment/MoviesListFragment.kt
+++ app/src/main/java/com/example/myapplicationtest/ui/fragment/MoviesListFragment.kt
@@ -1,25 +1,28 @@
 package com.example.myapplicationtest.ui.fragment
 
+import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import com.example.myapplicationtest.R
 
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
 class MoviesListFragment : Fragment() {
 
-    private var param1: String? = null
-    private var param2: String? = null
+    private var imageMovies: ImageView? = null
+    private var clickListener: ClickMovies? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
+    }
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        if (context is ClickMovies) {
+            clickListener = context
         }
     }
 
@@ -30,14 +33,29 @@ class MoviesListFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_movies_list, container, false)
     }
 
-    companion object {
-
-        fun newInstance() =
-            MoviesListFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initImageView(view)
     }
+
+    private fun initImageView(view: View) {
+        imageMovies = view.findViewById(R.id.ivMoviesList)
+        imageMovies?.setOnClickListener {
+            clickListener?.clickMoviesListener()
+        }
+    }
+
+    companion object {
+        fun newInstance() =
+            MoviesListFragment()
+    }
+
+    override fun onDetach() {
+        super.onDetach()
+        clickListener = null
+    }
+}
+
+interface ClickMovies {
+    fun clickMoviesListener()
 }
