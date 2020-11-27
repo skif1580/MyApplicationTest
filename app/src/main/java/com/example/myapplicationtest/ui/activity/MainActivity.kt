35c0f58e259141d1f8fc8ee958cc49cf03829d47
Xanter <skif1580.2@gmail.com>
2020-11-27 19:00:32 +0200

    format code

--- app/src/main/java/com/example/myapplicationtest/ui/activity/MainActivity.kt
+++ app/src/main/java/com/example/myapplicationtest/ui/activity/MainActivity.kt
@@ -13,7 +13,6 @@ class MainActivity : AppCompatActivity(), ClickMovies {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
         if (savedInstanceState == null) {
             supportFragmentManager.beginTransaction()
                 .replace(R.id.fl_container, MoviesListFragment.newInstance())
